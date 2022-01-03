a68c86b47da5b3e33b6a0e431b4c70c86b2fd59b
aghilann <90127404+aghilann@users.noreply.github.com>
2022-01-02 22:24:16 -0700

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,12 +4,9 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    #SECRET_KEY = 'a369a11e8a2aa2cf4c9ccfe957b3f6c8'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    #MAIL_USERNAME = "resetpassforflasksite@gmail.com"
-    #MAIL_PASSWORD = "dogdogdog"
