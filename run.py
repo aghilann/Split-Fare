afaae90f122713cee667098eaea037e455231ecf
aghilann <90127404+aghilann@users.noreply.github.com>
2022-01-02 19:48:35 -0700

    Update run.py

--- run.py
+++ run.py
@@ -17,6 +17,4 @@ from flaskblog import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False,
-            host='0.0.0.0',
-            port=int(os.environ.get('PORT', 2020))
+    app.run(debug=False)
