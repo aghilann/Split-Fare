a5339d61774aba91fbeeafe9c5763941262c03f0
aghilann <90127404+aghilann@users.noreply.github.com>
2022-01-02 19:30:37 -0700

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ def prod():
     app.run()
 
 if __name__ == '__main__':
-    app.run(debug=False
+    app.run(debug=False,
             host='0.0.0.0',
             port=int(os.environ.get('PORT', 2020))
 
