81fa9b82049e957a66012e2c6a00b51efee26e60
aghilann <90127404+aghilann@users.noreply.github.com>
2022-01-02 23:50:07 -0700

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
