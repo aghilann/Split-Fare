e7fe2151ac67f5a0bda9eb00f20fe3f5afc5e17d
aghilann <90127404+aghilann@users.noreply.github.com>
2022-01-02 19:38:23 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+'''
 from flaskblog import create_app
 from flask import Flask
 
@@ -5,6 +6,19 @@ app = Flask(__name__)
 
 def prod():
     app.run()
+
+if __name__ == '__main__':
+    app.run(debug=False,
+            host='0.0.0.0',
+            port=int(os.environ.get('PORT', 2020))
+prod()
+'''
+from flask import Flask
+from flaskblog import create_app
+app = Flask(__name__)
+
+def prod():
+    app.run(debug=True, use_reloader=True)
 
 if __name__ == '__main__':
     app.run(debug=False,
